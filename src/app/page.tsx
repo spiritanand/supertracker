6cb9f7c50f1f016bf5b8675130fbea1288b6928b
Suryansh Anand <spirit@anandsur.com>
2024-11-07 16:35:41 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default async function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-black">Hello World</h1>
+      <p className="font-se text-lg">This is a test</p>
+    </main>
+  );
+}
